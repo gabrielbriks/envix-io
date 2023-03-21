84f2dbe7eaf9cce3d5ae042f03433ea44319b399
Gabriel dos Reis Bispo de Morais <gabriel.morais@implantainformatica.com.br>
2023-03-21 11:06:45 -0300

    dev: Implementando novas rotas de testes com kenex
    Criado rota de insert, select e select com where

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,38 @@
 import fastify from 'fastify'
+import crypto from 'node:crypto'
 import { knex } from './database'
-
 const app = fastify()
 
 app.get('/hello', async () => {
   const tables = await knex('sqlite_schema').select('*')
 
   return tables
+})
+
+app.get('/new', async () => {
+  const transaction = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Transação de teste',
+      amount: 1000,
+    })
+    .returning('*') //Para retornar todas as informações que foram inseridas
+
+  return transaction
+})
+
+app.get('/get', async () => {
+  const transaction = await knex('transactions').select('*')
+
+  return transaction
+})
+
+app.get('/get/filter', async () => {
+  const transaction = await knex('transactions')
+    .where('amount', 1000)
+    .select('*')
+
+  return transaction
 })
 
 app
