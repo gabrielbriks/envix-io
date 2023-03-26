fab96090205c9ed56b7f5e4bd0b868f4e252c73b
Gabriel dos Reis Bispo de Morais <gabriel.morais@implantainformatica.com.br>
2023-03-26 19:31:57 -0300

    dev: Implementando validação de cookie e usando o preHandler
    - Implementa uso o preHandler(middleware) para validação da existência
    do cookie

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from 'fastify'
 import { randomUUID } from 'node:crypto'
 import { z } from 'zod'
 import { knex } from '../database'
+import { checkSessionIdExists } from '../middlewares/check-session-id-exists'
 
 export async function transactionsRoutes(app: FastifyInstance) {
   app.post('/', async (request, reply) => {
@@ -36,26 +37,41 @@ export async function transactionsRoutes(app: FastifyInstance) {
     return reply.status(201).send()
   })
 
-  app.get('/', async () => {
-    const transaction = await knex('transactions').select('*')
+  app.get('/', { preHandler: [checkSessionIdExists] }, async request => {
+    const { sessionId } = request.cookies
+
+    console.log('sessionId', sessionId)
+
+    const transaction = await knex('transactions')
+      .where('session_id', sessionId)
+      .select('*')
 
     return { transaction }
   })
 
-  app.get('/:id', async request => {
+  app.get('/:id', { preHandler: [checkSessionIdExists] }, async request => {
     const getTransactionParamsSchema = z.object({
       id: z.string().uuid(),
     })
 
     const { id } = getTransactionParamsSchema.parse(request.params)
+    const { sessionId } = request.cookies
 
-    const transaction = await knex('transactions').where('id', id).first()
+    const transaction = await knex('transactions')
+      .where({
+        id,
+        sessionId: sessionId,
+      })
+      .first()
 
     return { transaction }
   })
 
-  app.get('/summary', async () => {
+  app.get('/summary', { preHandler: [checkSessionIdExists] }, async request => {
+    const { sessionId } = request.cookies
+
     const summary = await knex('transactions')
+      .where('session_id', sessionId)
       .sum('amount', { as: 'amount' })
       .first()
 
