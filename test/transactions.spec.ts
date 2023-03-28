a582729cfafb3409984dd6ee7791d2d4be141085
Gabriel dos Reis Bispo de Morais <gabriel.morais@implantainformatica.com.br>
2023-03-28 19:42:52 -0300

    dev: Implementa separação de ambiente de tests e dev
    - Implementa execution e rollback de migrations a cada test
    - Implementa arquivo .env especifico para testes

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,6 +1,12 @@
+import { execSync } from 'node:child_process'
 import request from 'supertest'
-import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { afterAll, beforeAll, beforeEach, describe, expect, it } from 'vitest'
 import { app } from '../src/app'
+
+/**
+ * Testes E2E são como amigos -> Poucos e bons
+ *
+ *  */
 
 describe('Transactions Routes', () => {
   //Antes dos testes, aguarde o app carregar completamente
@@ -10,6 +16,11 @@ describe('Transactions Routes', () => {
 
   afterAll(async () => {
     await app.close()
+  })
+
+  beforeEach(async () => {
+    execSync('npm run knex migrate:rollback --all')
+    execSync('npm run knex migrate:latest')
   })
 
   it('should be able to create create  a new transaction', async () => {
